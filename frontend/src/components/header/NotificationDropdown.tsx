940fe1e7bf852c17adf93b485cf100790bae8367
Dennis Kimathi <kimathidennis38@gmail.com>
2025-08-18 12:45:04 +0300

    fix: fix lint errors

--- frontend/src/components/header/NotificationDropdown.tsx
+++ frontend/src/components/header/NotificationDropdown.tsx
@@ -1,13 +1,11 @@
 
 "use client";
-import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
 import { 
   DropdownMenu, 
   DropdownMenuContent, 
-  DropdownMenuTrigger, 
-  DropdownMenuItem 
+  DropdownMenuTrigger,  
 } from "@/components/ui/dropdown-menu";
 
 export default function NotificationDropdown() {
